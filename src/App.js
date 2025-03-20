ff9e9ba2e6c01b1c2521e6f6ca231f0484915ff1
kwasi <affulnana04@gmail.com>
2025-03-20 17:18:47 +0000

    add fleet typo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router"
 import Landing from "./pages/Landing"
+import Fleet from "./pages/Fleet"
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Landing/>} />
-      <Route path="/fleet" element={<fleet/>} />
+      <Route path="/fleet" element={<Fleet/>} />
     </Routes>
   </BrowserRouter>
   )
