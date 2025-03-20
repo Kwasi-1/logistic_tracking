d4fadc0d4a26acfe5e84eb1dc5d4d4cd653bbf8c
kwasi <affulnana04@gmail.com>
2025-03-20 15:06:47 +0000

    add app routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import MapComponent from "./components/MapComponent"
+import { BrowserRouter, Route, Routes } from "react-router"
+import Landing from "./pages/Landing"
 
 function App() {
   return (
-    <div>
-      <MapComponent />
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Landing/>} />
+      <Route path="/fleet" element={<fleet/>} />
+    </Routes>
+  </BrowserRouter>
   )
 }
 export default App
---
+++ src/pages/Landing.js
@@ -0,0 +1,10 @@
+import MapComponent from "../components/MapComponent"
+
+function landing() {
+  return (
+    <div>
+      <MapComponent />
+    </div>
+  )
+}
+export default landing
