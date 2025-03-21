35c20ade0991f1f8e8998a629da38a67c91f7255
kwasi <affulnana04@gmail.com>
2025-03-21 15:14:29 +0000

    add react chart

--- src/components/fleet_management/RangeBar.jsx
+++ src/components/fleet_management/RangeBar.jsx
@@ -2,7 +2,7 @@ const RangeBar = ({ value, total }) => {
   const percentage = (value / total) * 100;
 
   return (
-    <div className="w-full rounded-xl h-4 bg-gray-200 mt-2">
+    <div className="w-full rounded-xl h-2 bg-gray-200 mt-2">
       <div
         className="h-full rounded-xl min-w-[7px] transition-all duration-300"
         style={{
