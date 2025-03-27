fa72d19bfd0365032755840e383e13b76419eb0a
kwasi <affulnana04@gmail.com>
2025-03-27 14:17:41 +0000

    fix drivers text

--- src/pages/DriverDetails.jsx
+++ src/pages/DriverDetails.jsx
@@ -3,7 +3,7 @@ import DriverTable from "../components/fleet_management/DriverTable";
 function DriverDetails() {
   return (
     <div className="px-8 h-screen">
-      <h1 className="py-5 font-semibold text-2xl">Shipment</h1>
+      <h1 className="py-5 font-semibold text-2xl">Drivers</h1>
       <div className="mt4 bg-gray-200/30 h-full rounded-t-xl border border-[#e0e6e930]">
         <DriverTable />
       </div>
