6a01ea7b55f08aff9e01f6035b854856ef7da1c5
kwasi <affulnana04@gmail.com>
2025-04-03 15:10:04 +0000

    fix this edit hstory page

--- src/components/fleet_management/driver/EditsHistory.jsx
+++ src/components/fleet_management/driver/EditsHistory.jsx
@@ -25,17 +25,14 @@ const EditsHistory = () => {
         <table className="w-full text-left border-collapse">
           <thead>
             <tr className="text-gray-600 text-xs border-b border-gray-300">
-              <th className="p-2">EDITED BY ⬍</th>
-              <th className="p-2">EDIT DATE ⬍</th>
-              <th className="p-2">COMMENTS ⬍</th>
+              <th className="p-2">EDITED BY </th>
+              <th className="p-2">EDIT DATE </th>
+              <th className="p-2">COMMENTS </th>
             </tr>
           </thead>
           <tbody>
             {editHistory.map((edit, index) => (
-              <tr
-                key={index}
-                className="border-b text-xs text-gray-500"
-              >
+              <tr key={index} className="border-b text-xs text-gray-500">
                 <td className="p-2">{edit.editor}</td>
                 <td className="p-2">{edit.date}</td>
                 <td className="p-2">{edit.comment}</td>
